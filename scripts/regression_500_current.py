eff4b0e87809414ba93dfd49b755dd73bbfd5c4a
Bambale0 <83171474+Bambale0@users.noreply.github.com>
2026-07-28 12:59:18 +0300

    Run Mini App request integrity regression

--- scripts/regression_500_current.py
+++ scripts/regression_500_current.py
@@ -11,6 +11,7 @@ from scripts import current_policy_regression_adapter as adapter
 from scripts.current_model_policy_patch import install
 from scripts.private_feed_policy_patch import install as install_private_feed_policy
 from scripts.regression_dynamic_packages import run_dynamic_package_regression
+from scripts.regression_tma_request_integrity import run_tma_request_integrity_regression
 
 install(adapter)
 install_private_feed_policy(adapter)
@@ -18,6 +19,7 @@ install_private_feed_policy(adapter)
 
 async def amain() -> None:
     await run_dynamic_package_regression()
+    await run_tma_request_integrity_regression()
     await adapter.amain()
 
 
